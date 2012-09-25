333f35fc27369f6e8c2d4e0b1f6fc0ee97a18565
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2012-09-25 15:26:42 -0400

    Added check to see if store was specified.

--- src/main/webapp/resources/app/Tool.js
+++ src/main/webapp/resources/app/Tool.js
@@ -3,11 +3,13 @@ Ext.define('Voyant.Tool', {
 	
 	constructor: function() {
 		// check if global store exists, otherwise use local store
-		var store = Ext.getStore(this.store);
-		if (store == undefined) {
-			this.store = Ext.create('Voyant.store.'+this.store);
+		if (this.store) {
+			var store = Ext.getStore(this.store);
+			if (store == undefined) {
+				this.store = Ext.create('Voyant.store.'+this.store);
+			}
 		}
 		
-		Voyant.Tool.superclass.constructor.call(this);
+		this.callParent();
 	}
 });
