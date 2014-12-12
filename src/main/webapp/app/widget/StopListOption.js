5ecc5426e5700ca130ef642952738d4f224b1ab0
sgsinclair <sgsinclair@gmail.com>
2014-12-11 22:46:21 -0500

    preliminary stoplist editing

--- src/main/webapp/app/widget/StopListOption.js
+++ src/main/webapp/app/widget/StopListOption.js
@@ -7,7 +7,10 @@ Ext.define('Voyant.widget.StopListOption', {
     	i18n: {
     		label: {en: "Stopwords:"},
     		editList: {en: "Edit List"},
+    		noEditAutoTitle: {en: "Edit Stoplist Error"},
+    		noEditAutoMessage: {en: 'The auto-detected stoplist cannot be edited, please select a specifc stoplist such as the "New User-Defined List".'},
     		auto: {en: "Auto-detect"},
+    		'new': {en: "New User-Defined List"},
     		en: {en: "English"},
     		de: {en: "German"},
     		es: {en: "Spanish"},
@@ -16,14 +19,15 @@ Ext.define('Voyant.widget.StopListOption', {
     		it: {en: "Italian"},
     		no: {en: "Norwegian"},
     		se: {en: "Swedish"},
-    		mu: {en: "Multilingual"}
+    		mu: {en: "Multilingual"},
+    		ok: {en: "Save"},
+    		cancel: {en: "Cancel"}
     	}
     },
     initComponent: function(config) {
     	var me = this;
     	var value = this.up('window').panel.getApiParam('stopList');
-        var data = [{name : this.localize('auto'),   value: 'auto'},
-               {name : this.localize('en'),   value: 'stop.en.taporware.txt'},
+        var data = [{name : this.localize('en'),   value: 'stop.en.taporware.txt'},
                {name : this.localize('de'),   value: 'stop.de.german.txt'},
                {name : this.localize('es'),   value: 'stop.es.spanish.txt'},
                {name : this.localize('fr'),   value: 'stop.fr.veronis.txt'},
@@ -35,6 +39,7 @@ Ext.define('Voyant.widget.StopListOption', {
     	data.sort(function(a,b) { // sort by label
     		return a.name < b.name ? -1 : 1;
     	})
+    	data.splice(0, 0, {name : this.localize('auto'),   value: 'auto'}, {name : this.localize('new'),   value: 'new'})
     	
     	Ext.apply(me, {
 	    		items: [{
@@ -52,11 +57,109 @@ Ext.define('Voyant.widget.StopListOption', {
 	    	            fields: ['name', 'value'],
 	    	            data: data
 	    	        }
-	    		}/*,{
+	    		},{
 	    			xtype: 'button',
-	    			text: this.localize('editList')
-	    		}*/]
+	    			text: this.localize('editList'),
+	    			handler: this.editList,
+	    			scope: this
+	    		}]
     	})
         me.callParent(arguments);
+    },
+    
+    editList: function() {
+    	var win = this.up('window');
+    	var panel = win.panel;
+    	var value = this.down('combo').getValue();
+    	var corpusId = panel.getApplication && panel.getApplication().getCorpus ? panel.getApplication().getCorpus().getId() : undefined;
+    	if (value=='auto' && !corpusId) {
+    		Ext.Msg.show({
+			    title: this.localize('noEditAutoTitle'),
+			    message: this.localize('noEditAutoMessage'),
+			    buttons: Ext.Msg.OK,
+			    icon: Ext.Msg.ERROR,
+			});
+    		return
+    	}
+    	Ext.Ajax.request({
+    	    url: panel.getTromboneUrl(),
+    	    params: {
+        		tool: 'resource.KeywordsManager',
+    			stopList: value,
+    			corpus: corpusId
+    	    },
+    	    success: function(response){
+    	    	var json = Ext.util.JSON.decode(response.responseText);
+    	    	var keywords = json.keywords.keywords.join("\n");
+    			Ext.Msg.show({
+	    		    title: this.localize('editStopListTitle'),
+	    		    message: this.localize('editStopListMessage'),
+	    		    buttons: Ext.Msg.OKCANCEL,
+	    		    buttonText: {
+	    		        ok: this.localize('ok'),
+	    		        cancel: this.localize('cancel')
+	    		    },
+	    		    icon: Ext.Msg.INFO,
+	    		    prompt: true,
+	    	        multiline: true,
+	    	        value: keywords,
+	    	        original: keywords,
+	    	        fn: function(btn,value,stoplist) {
+	    	        	if (btn=='ok' && stoplist.original!=value) {
+	    	        		var combo = this.down('combo')
+	    	        		if (Ext.String.trim(value).length==0) {
+	    	        			combo.setValue('empty');
+	    	        		}
+	    	        		else {
+	    	        	    	Ext.Ajax.request({
+	    	        	    	    url: panel.getTromboneUrl(),
+	    	        	    	    params: {
+	    	        	        		tool: 'resource.StoredResource',
+	    	        	    			storeResource: value,
+	    	        	    			corpus: corpusId
+	    	        	    	    },
+	    	        	    	    combo: combo,
+	    	        	    	    success: function(response, req) {
+	    	        	    	    	var json = Ext.util.JSON.decode(response.responseText);
+	    	        	    	    	var store = req.combo.getStore();
+	    	        	    	    	var value = 'keywords-'+json.storedResource.id;
+	    	        	    	    	store.add({name: 'Added '+Ext.Date.format(new Date(),'M j, g:i:s a'), value: value});
+	    	        	    	    	req.combo.setValue(value)
+	    	        	    	    	req.combo.updateLayout()
+	    	        	    	    },
+	    	        	    	    scope: this
+	    	        	    	})
+	    	        		}
+	    	        	}
+	    	        },
+	    	        scope: this
+    			})
+    	    },
+    	    scope: this
+    	});
+    	
+//    	$.getJSON( this.up('window').panel.getTromboneUrl(), {
+//    		tool: 'resource.KeywordsManager',
+//			stopList: this.down('combo').getValue()
+//    	}).done(function(data) {
+//    		deb
+//    		this.unmask();
+//    	}).fail(function() {
+//    		debugger
+//    	})
+//		Ext.Msg.show({
+//		    title: this.localize('exportDataTitle'),
+//		    message: this.localize('exportDataHtmlMessage'),
+//		    buttons: Ext.Msg.OK,
+//		    icon: Ext.Msg.INFO,
+//		    prompt: true,
+//	        multiline: true,
+//	        value: '',
+//	        listeners: {
+//	        	render: function() {
+//	        		debugger
+//	        	}
+//	        }
+//		});
     }
 })
