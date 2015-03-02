d755598c0a2c224328da934264c737cd4efeb26b
sgsinclair <sgsinclair@gmail.com>
2015-03-02 14:20:59 -0500

    better handling of file types in UI

--- src/main/webapp/app/panel/CorpusCreator.js
+++ src/main/webapp/app/panel/CorpusCreator.js
@@ -10,6 +10,11 @@ Ext.define('Voyant.panel.CorpusCreator', {
     		helpTip: {en: "This tool allows you to create a corpus in one of three ways:<ol><li>by typing or pasting text into the text box and clicking <i>Reveal</i>; if each line in the text box is a URL, text is fetched from those URLs, otherwise the contents are treated as a single document</li><li>click the <i>Open</i> button to open an existing corpus</li><li>click the <i>Upload</i> button to upload one or more files from you computer (you can select multiple files by using the Ctrl and/or Shift keys)</li></ul>"},
     		emptyInput: {en: "Type in one or more URLs on separate lines or paste in a full text."},
     		uploadingCorpus: {en: "Uploading corpus…"},
+    		fileTypesWarning: {en: "File Types Warning"},
+    		fileTypesMessage: {en: "You have one or more files with incompatible or unrecognized file extensions that may cause problems."},
+    		badFiles: {en: "incompatible (likely error): "},
+    		unknownFiles: {en: "unrecognized (possible error): "},
+    		sureContinue: {en: "Are you sure you wish to continue?"},
     		reveal: {en: "Reveal"}
     	}
     },
@@ -119,19 +124,46 @@ Ext.define('Voyant.panel.CorpusCreator', {
         	            change: function(filefield, value) {
         	            	if (value) {
             	            	var form = filefield.up('form').getForm();
-            	            	var view = me.getApplication().getViewport();
             	            	if (form.isValid()) {
-            	            		view.mask(me.localize('uploadingCorpus'))
-            	            		form.submit({
-            	            			url: me.getTromboneUrl(),
-            	            			params: {tool: 'corpus.CorpusCreator'},
-            	            			failure: function(form, action) { // we always fail because of content-type
-                        	            	view.unmask()
-            	            				if (action.result) {
-            	            					me.loadCorpus({corpus: action.result.stepEnabledCorpusCreator.storedId})
-            	            				}
+            	            		var files = filefield.fileInputEl.dom.files;
+            	            		var badFilesRe = /\.(png|gif|jpe?g|xls|mp[234a]|mpeg|exe|wmv|avi|ppt|mpg|tif|wav|mov|psd|wma|ai|bmp|pps|aif|pub|dwg|indd|swf|asf|mbd|dmg|flv)$/i
+            	            		var goodFilesRe = /\.(pdf|html?|xml|docx?|rtf|pages|odt|zip|jar|tar|gz|ar|cpio|bzip2|bz2|gzip)$/i
+            	            		var badFiles = [];
+            	            		var unknownFiles = [];
+            	            		for (var i = 0, len = files.length; i<len; i++) {
+            	            			var filename = files[i].name;
+            	            			if (badFilesRe.test(filename)) {
+            	            				badFiles.push(filename.split("/").pop())
             	            			}
-            	            		})
+            	            			else if (!goodFilesRe.test(filename)) {
+            	            				unknownFiles.push(filename.split("/").pop())
+            	            			}
+            	            		}
+            	            		if (badFiles || unknownFiles) {
+            	            			var file = filefield;
+            	            			Ext.Msg.show({
+            	            				title: me.localize("fileTypesWarning"),
+            	            				icon: Ext.MessageBox.ERROR,
+            	            				message: me.localize('fileTypesMessage')+'<ul>' +
+            	            					(badFiles.length > 0 ? ('<li>'+me.localize("badFiles") + badFiles.slice(0, 5).join(", ") + (badFiles.length>5 ? '…' : '') + '</li>') : '') +
+            	            					(unknownFiles.length>0 ? ('<li>' +me.localize("unknownFiles") + unknownFiles.slice(0, 5).join(", ") + (unknownFiles.length>5 ? '…' : '') +'</li>') : '')+
+            	            					'</ul>'+me.localize('sureContinue'),
+            	            				buttons: Ext.Msg.YESNO,
+            	            				fn: function(btn) {
+            	            			        if (btn === 'yes') {
+            	            			        	me.loadForm(form);
+            	            			        }
+            	            			        else {
+            	            			        	file.reset(); // make sure we can trigger a change next time
+            	            			        	file.fileInputEl.dom.setAttribute('multiple', true);
+            	            			        }
+            	            			    },
+            	            				scope: form
+            	            			});
+            	            		}
+            	            		else {
+            	            			me.loadForm(form);
+            	            		}
             	            	}
         	            	}
         	            }
@@ -169,6 +201,22 @@ Ext.define('Voyant.panel.CorpusCreator', {
         me.callParent(arguments);
     },
     
+    loadForm: function(form) {
+    	var view = this.getApplication().getViewport();
+		view.mask(this.localize('uploadingCorpus'))
+		form.submit({
+			url: this.getTromboneUrl(),
+			params: {tool: 'corpus.CorpusCreator'},
+			failure: function(form, action) { // we always fail because of content-type
+            	view.unmask()
+				if (action.result) {
+					this.loadCorpus({corpus: action.result.stepEnabledCorpusCreator.storedId})
+				}
+			},
+			scope: this
+		})
+    },
+   
     loadCorpus: function(params) {
 		var app = this.getApplication();
     	var view = app.getViewport();
