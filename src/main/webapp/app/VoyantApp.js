a530f07a23db2d7617e59a8edffb4d92bac25538
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2015-10-19 16:48:08 -0400

    Added missing Voyant.util.Api mixin

--- src/main/webapp/app/VoyantApp.js
+++ src/main/webapp/app/VoyantApp.js
@@ -1,7 +1,7 @@
 Ext.define('Voyant.VoyantApp', {
 	
     extend: 'Ext.app.Application',
-	mixins: ['Voyant.util.Deferrable','Voyant.util.Localization'],
+	mixins: ['Voyant.util.Deferrable','Voyant.util.Localization','Voyant.util.Api'],
 	requires: ['Voyant.util.ResponseError'],
     
     name: 'VoyantApp',
