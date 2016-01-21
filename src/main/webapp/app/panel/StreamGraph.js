89064b2c20c53350f7e37faab31c78efe2fd89ef
Andrew MacDonald <andrew_james_macdonald@yahoo.com>
2016-01-21 15:19:51 -0500

    StreamGraph
    -better axes
    -freqs selector

--- src/main/webapp/app/panel/StreamGraph.js
+++ src/main/webapp/app/panel/StreamGraph.js
@@ -5,15 +5,20 @@ Ext.define('Voyant.panel.StreamGraph', {
     statics: {
     	i18n: {
     		title: {en: 'StreamGraph'},
-    		
+    		freqsMode: {en: 'Frequencies'},
+    		freqsModeTip: {en: 'Determines if frequencies are expressed as raw counts or as relative counts (per document or segment).'},
+    		rawFrequencies: {en: 'Raw Frequencies'},
+    		relativeFrequencies: {en: 'Relative Frequencies'},
+    		documentSegments: {en: 'Document Segments'},
+    		documents: {en: 'Documents'},
     		clearTerms : {en: 'Clear Terms'},
-    		granularity : {en: 'Granularity'}
+    		segments : {en: 'Segments'}
     	},
     	api: {
     		limit: 5,
     		stopList: 'auto',
     		query: undefined,
-    		withDistributions: 'raw',
+    		withDistributions: 'relative',
     		bins: 50,
     		docIndex: undefined,
     		docId: undefined
@@ -28,7 +33,7 @@ Ext.define('Voyant.panel.StreamGraph', {
     	mode: undefined
     },
     
-    graphMargin: {top: 10, right: 10, bottom: 50, left: 50},
+    graphMargin: {top: 20, right: 60, bottom: 110, left: 80},
     
     MODE_CORPUS: 'corpus',
     MODE_DOCUMENT: 'document',
@@ -45,7 +50,7 @@ Ext.define('Voyant.panel.StreamGraph', {
     		title: this.localize('title'),
     		tbar: new Ext.Toolbar({
         		enableOverflow: true,
-				items: {
+				items: ['->',{
 					xtype: 'legend',
 					store: new Ext.data.JsonStore({
 						fields: ['name', 'mark', 'active']
@@ -60,28 +65,60 @@ Ext.define('Voyant.panel.StreamGraph', {
 						},
 						scope: this
 					}
-				}
+				},'->']
 			}),
 			bbar: new Ext.Toolbar({
         		enableOverflow: true,
 				items: [{
                 	xtype: 'querysearchfield'
-                },{
-	            	xtype: 'button',
-	            	text: this.localize('clearTerms'),
-	            	handler: function() {
-	            		
-	            	},
-	            	scope: this
-	            },
-	            '-',{
+                },
+//                {
+//	            	xtype: 'button',
+//	            	text: this.localize('clearTerms'),
+//	            	handler: function() {
+//	            		this.setApiParams({query: undefined, limit: 5, stopList: 'auto'});
+//						this.loadFromCorpus();
+//	            	},
+//	            	scope: this
+//	            },
+	            {
 	            	xtype: 'corpusdocumentselector',
 	            	singleSelect: true
-	            }
-	            ,'-',{
+	            },{
+	            	text: this.localize('freqsMode'),
+					glyph: 'xf201@FontAwesome',
+				    tooltip: this.localize('freqsModeTip'),
+				    menu: {
+				    	items: [{
+				               text: this.localize('relativeFrequencies'),
+				               checked: true,
+				               itemId: 'relative',
+				               group: 'freqsMode',
+				               checkHandler: function(item, checked) {
+				            	   if (checked) {
+				                	   this.setApiParam('withDistributions', 'relative');
+				                	   this.loadFromCorpus();
+				            	   }
+				               },
+				               scope: this
+				           }, {
+				               text: this.localize('rawFrequencies'),
+				               checked: false,
+				               itemId: 'raw',
+				               group: 'freqsMode',
+				               checkHandler: function(item, checked) {
+				            	   if (checked) {
+				                	   this.setApiParam('withDistributions', 'raw');
+				                	   this.loadFromCorpus();
+				            	   }
+				               },
+				               scope: this
+			           }]
+				    }
+	            },{
 	            	xtype: 'slider',
-	            	itemId: 'granularity',
-	            	fieldLabel: this.localize('granularity'),
+	            	itemId: 'segmentsSlider',
+	            	fieldLabel: this.localize('segments'),
 	            	labelAlign: 'right',
 	            	labelWidth: 70,
 	            	width: 150,
@@ -89,6 +126,9 @@ Ext.define('Voyant.panel.StreamGraph', {
 	            	minValue: 10,
 	            	maxValue: 300,
 	            	listeners: {
+	            		afterrender: function(slider) {
+	            			slider.setValue(this.getApiParam('bins'));
+	            		},
 	            		changecomplete: function(slider, newvalue) {
 	            			this.setApiParams({bins: newvalue});
 	            			this.loadFromCorpus();
@@ -127,7 +167,7 @@ Ext.define('Voyant.panel.StreamGraph', {
 		this.on('query', function(src, query) {
         	var terms = this.getCurrentTerms();
         	terms.push(query);
-        	this.setApiParams({query: terms, limit: terms.length});
+        	this.setApiParams({query: terms, limit: terms.length, stopList: undefined});
         	if (this.getMode() === this.MODE_DOCUMENT) {
         		this.loadFromDocumentTerms();
         	} else {
@@ -230,17 +270,18 @@ Ext.define('Voyant.panel.StreamGraph', {
     	
     	var steps;
     	if (this.getMode() === this.MODE_DOCUMENT) {
-    		steps = this.getApiParam('bins')-1;
+    		steps = this.getApiParam('bins');
     	} else {
-    		steps = this.getCorpus().getDocumentsCount()-1;
+    		steps = this.getCorpus().getDocumentsCount();
     	}
+    	steps--;
+    	
     	var width = this.body.down('svg').getWidth() - this.graphMargin.left - this.graphMargin.right;
     	var x = d3.scale.linear().domain([0, steps]).range([0, width]);
     	
     	var max = d3.max(processedLayers, function(layer) {
     		return d3.max(layer, function(d) { return d.y0 + d.y; });
     	});
-    	
     	var height = this.body.down('svg').getHeight() - this.graphMargin.top - this.graphMargin.bottom;
     	var y = d3.scale.linear().domain([0, max]).range([height, 0]);
     	
@@ -249,16 +290,11 @@ Ext.define('Voyant.panel.StreamGraph', {
 			.y0(function(d) { return y(d.y0); })
 			.y1(function(d) { return y(d.y0 + d.y); });
     	
-    	var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(steps);
+    	var xAxis = d3.svg.axis().scale(x).orient('bottom');
     	if (this.getMode() === this.MODE_CORPUS) {
-    		// TODO change x scale to ordinal for corpus mode
-    		var docLabels = [];
-    		this.getCorpus().getDocuments().each(function(doc) {
-    			docLabels.push(doc.get('index'));//getTinyTitle());
-    		});
-    		xAxis.tickValues(docLabels);
+    		xAxis.tickFormat(''); // hide tick numbers
     	}
-//    	
+    	
     	var yAxis = d3.svg.axis().scale(y).orient('left');
     	
     	// join
@@ -282,10 +318,44 @@ Ext.define('Voyant.panel.StreamGraph', {
     		.attr('class', 'axis x')
     		.attr('transform', 'translate(0,'+height+')')
     		.call(xAxis);
+    	
+    	var xAxisText;
+    	if (this.getMode() === this.MODE_CORPUS) {
+    		var stepIncrement = width / steps;
+    		var currStep = 0;
+    		this.getCorpus().getDocuments().each(function(doc) {
+    			this.getVis().select('g.x').append("text")
+					.attr('text-anchor', 'end')
+					.attr('transform', 'translate('+currStep+', 10) rotate(-45)')
+					.text(doc.getTinyTitle());
+    			
+    			currStep += stepIncrement;
+    		}, this);
+    		
+    		xAxisText = this.localize('documents');
+    	} else {
+    		xAxisText = this.localize('documentSegments');
+    	}
+    	this.getVis().select('g.x').append("text")
+			.attr('text-anchor', 'middle')
+			.attr('transform', 'translate('+width/2+', '+(this.graphMargin.bottom-20)+')')
+			.text(xAxisText);
+    	
     	this.getVis().append('g')
 			.attr('class', 'axis y')
 			.attr('transform', 'translate(0,0)')
 			.call(yAxis);
+    	
+    	var yAxisText;
+    	if (this.getApiParam('withDistributions') === 'raw') {
+    		yAxisText = this.localize('rawFrequencies');
+    	} else {
+    		yAxisText = this.localize('relativeFrequencies');
+    	}
+    	this.getVis().select('g.y').append("text")
+			.attr('text-anchor', 'middle')
+			.attr('transform', 'translate(-'+(this.graphMargin.left-20)+', '+height/2+') rotate(-90)')
+			.text(yAxisText);
     },
     
 	getCurrentTerms: function() {
@@ -317,6 +387,10 @@ Ext.define('Voyant.panel.StreamGraph', {
 					.attr('width', width+paddingH).attr('height', height+paddingV).append('g').attr('transform', 'translate('+this.graphMargin.left+','+this.graphMargin.top+')')
 			);
     	}
+    },
+    
+    updateMode: function(mode) {
+    	this.queryById('segmentsSlider').setHidden(mode==this.MODE_CORPUS);
     }
 });
 
