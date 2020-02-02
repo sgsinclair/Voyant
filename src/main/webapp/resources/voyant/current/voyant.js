5bef145e0535bd28bf08c77bdfe1e90b161f1c37
Stéfan Sinclair <sgsinclair@gmail.com>
2020-02-01 20:34:52 -0500

    improvements to inserting html and Corpus.words()

--- src/main/webapp/resources/voyant/current/voyant.js
+++ src/main/webapp/resources/voyant/current/voyant.js
@@ -1,4 +1,4 @@
-/* This file created by JSCacher. Last modified: Thu Jan 30 16:27:13 EST 2020 */
+/* This file created by JSCacher. Last modified: Sat Feb 01 16:38:22 EST 2020 */
 function Bubblelines(config) {
 	this.container = config.container;
 	this.externalClickHandler = config.clickHandler;
@@ -35323,7 +35323,7 @@ Ext.define("Voyant.notebook.editor.TextEditor", {
 	alias: "widget.notebooktexteditor",
 	cls: 'notebook-text-editor',
 	config: {
-		ckeditorConfig : { // when upgrading ckeditor, remember to copy stopediting plugin
+		ckeditorConfig : { // when upgrading ckeditor, remember to copy stopediting and inserthtml4x plugins
 			toolbar:  [
 				    	{ name: 'basicstyles', items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
 				    	{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Justify', 'Outdent', 'Indent', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
@@ -35331,10 +35331,11 @@ Ext.define("Voyant.notebook.editor.TextEditor", {
 				    	{ name: 'styles', items: [ 'Styles', 'Format' ] },
 				    	{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor'] },
 				    	{ name: 'insert', items: [ 'Image', 'Table' ] },
-				    	{ name: 'document', items: [ 'Sourcedialog', 'Stopediting'] }
+				    	{ name: 'document', items: [ 'inserthtml4x', 'Sourcedialog', 'Stopediting'] }
 		    ],
-		    extraPlugins: 'stopediting,sourcedialog,justify,colorbutton',
-		    removePlugins: 'iframe',
+		    
+		    extraPlugins: 'stopediting,sourcedialog,justify,colorbutton,inserthtml4x',
+//		    removePlugins: 'iframe',
 			allowedContent: true,
 			toolbarCanCollapse: true,
 			startupFocus: true
