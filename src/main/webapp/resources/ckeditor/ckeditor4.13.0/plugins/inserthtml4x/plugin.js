441ed8159be10f10b64fa23e62b3daa8c1948a18
Andrew James <ajmacdonald@users.noreply.github.com>
2020-06-10 12:06:41 -0400

    add inserthtml4x files

---
+++ src/main/webapp/resources/ckeditor/ckeditor4.13.0/plugins/inserthtml4x/plugin.js
@@ -0,0 +1,30 @@
+﻿/*********************************************************************************************************/
+/**
+ * inserthtml plugin for CKEditor 4.x (Author: gpickin ; email: gpickin@gmail.com)
+ * version:	2.0
+ * Released: On 2015-03-10
+ * Download: http://www.github.com/gpickin/ckeditor-inserthtml 
+ *
+ *
+ * Modified from original: inserthtml plugin for CKEditor 3.x (Author: Lajox ; Email: lajox@19www.com)
+ * mod-version:	 1.0
+ * mod-Released: On 2009-12-11
+ * mod-Download: http://code.google.com/p/lajox
+ */
+/*********************************************************************************************************/
+
+CKEDITOR.plugins.add('inserthtml4x',   
+  {    
+    icons: 'inserthtml4x',
+    init: function( editor ) { 
+		editor.addCommand( 'inserthtml4x', new CKEDITOR.dialogCommand( 'inserthtmlDialog' ) );
+        editor.ui.addButton( 'inserthtml4x', {
+            label: 'Insert HTML',
+            command: 'inserthtml4x',
+            toolbar: 'insert',
+            icon : this.path + 'inserthtml.png'
+        });
+
+        CKEDITOR.dialog.add( 'inserthtmlDialog', this.path + 'dialogs/inserthtml.js' );
+    }    
+});
