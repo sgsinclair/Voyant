55114ff0f8a7e9639fd36bbe13b82edb2f5b2406
Andrew James <ajmacdonald@users.noreply.github.com>
2021-01-05 14:18:45 -0500

    docs: windows version script, config changes

--- src/main/webapp/jsduck.sh
+++ src/main/webapp/jsduck.sh
@@ -1,9 +1,8 @@
-jsduck --config resources/voyant/current/docs/en/config.json --output docs --ignore-global app; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js; jsduck --config resources/voyant/current/docs/en/config.json --output docs/ace --export=full --ignore-global app
-
-jsduck --config resources/voyant/current/docs/en/config.json --output docs --ignore-global; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js;
-
-jsduck --verbose -processes=1 --config resources/voyant/current/docs/en/config.json --output docs --ignore-global app; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js
-
+# generate docs (no spyral)
+#/home/andrew/.gem/bin/solvas-jsduck --config resources/voyant/current/docs/en/config.json --output docs; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js;
+#
+# generate spyral source for api
 python3 resources/spyral/src-jsduck/jsdocs.py --input-dir ../../../../voyantjs/src resources/spyral/src --output-file resources/spyral/src-jsduck/spyral.js --exclude-file resources/spyral/src/index.js
-
-jsduck --no-source --verbose --processes=1 --config resources/voyant/current/docs/en/config.json --output docs --ignore-global resources/spyral/src-jsduck/; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js
+#
+# generate docs (with spyral)
+/home/andrew/.gem/bin/solvas-jsduck --no-source --verbose --processes=1 --config resources/voyant/current/docs/en/config.json --output docs --ignore-global resources/spyral/src-jsduck/; cp docs/index.html docs/index.jsp; chmod 644 docs/extjs/ext-all.js;
