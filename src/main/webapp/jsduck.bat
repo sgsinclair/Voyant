3f80814610fafdcac075d2c68514d6ad75349bbf
Andrew James <ajmacdonald@users.noreply.github.com>
2021-02-01 12:56:36 -0500

    docs: change jsduck version

--- src/main/webapp/jsduck.bat
+++ src/main/webapp/jsduck.bat
@@ -1,8 +1,8 @@
 :: generate docs (no spyral)
-@REM ruby C:\Ruby27-x64\lib\ruby\gems\2.7.0\gems\solvas-jsduck-6.0.0.30539\bin\solvas-jsduck --config resources\voyant\current\docs\en\config.json --output docs && copy docs\index.html docs\index.jsp
+@REM ruby C:\Ruby27-x64\lib\ruby\gems\2.7.0\gems\jsduck-5.3.4\bin\jsduck --config resources\voyant\current\docs\en\config.json --output docs && copy docs\index.html docs\index.jsp
 
 :: generate spyral source for api
 python resources\spyral\src-jsduck\jsdocs.py --input-dir ..\..\..\..\voyantjs\src resources\spyral\src --output-file resources\spyral\src-jsduck\spyral.js --exclude-file resources\spyral\src\index.js
 
 :: generate docs (with spyral)
-ruby C:\Ruby27-x64\lib\ruby\gems\2.7.0\gems\solvas-jsduck-6.0.0.30539\bin\solvas-jsduck --no-source --verbose --config resources\voyant\current\docs\en\config.json --output docs --ignore-global resources\spyral\src-jsduck\ && copy docs\index.html docs\index.jsp
+ruby C:\Ruby27-x64\lib\ruby\gems\2.7.0\gems\jsduck-5.3.4\bin\jsduck --no-source --verbose --warnings=-image_unused --config resources\voyant\current\docs\en\config.json --output docs --ignore-global resources\spyral\src-jsduck\ && copy docs\index.html docs\index.jsp
